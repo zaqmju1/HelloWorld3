5e5fb11990c2ca3f4d1e0e18d808e9f0efc651c5
Jesse <jesse.henderson@maine.edu>
2019-02-04 11:03:11 -0500

    Added comment

--- Hello/HelloMain.java
+++ Hello/HelloMain.java
@@ -2,6 +2,7 @@ package Hello;
 
 public class HelloMain {
 
+    // Print program
     public static void main(String[] args) {
         
         System.out.println("Here ye : important announcement -- on branch ");
