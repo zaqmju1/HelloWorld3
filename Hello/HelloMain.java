7d68fa59209a566d084257ee8c101af7e2e54882
Jesse <jesse.henderson@maine.edu>
2019-02-04 12:56:23 -0500

    Alice commit

--- Hello/HelloMain.java
+++ Hello/HelloMain.java
@@ -9,6 +9,9 @@ public class HelloMain {
         
         System.out.println("Hello World");
         
+        // Alice addition
+        System.out.println("Hi Alice");
+        
         // Added content
         System.out.println("Goodbye");
     }
